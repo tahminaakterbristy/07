72b36ff9689674184127b35355082cd412f0c780
tahminaakterbristy <38464255+tahminaakterbristy@users.noreply.github.com>
2024-09-16 00:21:05 +0600

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+
+import './App.css'
+import Navbar from './component/Navbar/Navbar'
+
+function App() {
+
+
+  return (
+    <div>
+     <Navbar></Navbar>
+      
+    </div>
+  )
+}
+
+export default App
